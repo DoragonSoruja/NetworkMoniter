b96e94e5151d0ac14eead75e459ad6571fb0eb00
Sebastian Roberts <tiansebas09@gmail.com>
2020-06-23 14:50:44 -0500

    Basic Program

--- NetworkManager/Program.cs
+++ NetworkManager/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.NetworkInformation;
 
 namespace NetworkManager
 {
@@ -17,6 +18,27 @@ namespace NetworkManager
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            BytesSentAndReceived();
+        }
+
+        static void BytesSentAndReceived()
+        {
+            if (!NetworkInterface.GetIsNetworkAvailable())
+            {
+                Console.WriteLine("Failed");
+                return;
+            }
+
+            NetworkInterface[] interfaces = NetworkInterface.GetAllNetworkInterfaces();
+
+            foreach (NetworkInterface ni in interfaces)
+            {
+                Console.WriteLine(ni.Description);
+                Console.WriteLine("    Bytes Sent: {0}", ni.GetIPv4Statistics().BytesSent);
+                Console.WriteLine("    Bytes Received: {0}", ni.GetIPv4Statistics().BytesReceived);
+            }
+
+            string filler = Console.ReadLine();
         }
     }
 }
